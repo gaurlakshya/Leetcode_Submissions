a615936c696ceb14239b68e3ae262f90d6826d46
gaurlakshya <69047854+gaurlakshya@users.noreply.github.com>
2022-06-24 22:24:25 +0530

    Time: 5 ms (28.44%), Space: 6.2 MB (23.36%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int titleToNumber(string columnTitle) {
         int n=columnTitle.length();
-        vector<char> vec={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
         int ans=0;
         if(n==1)
             return columnTitle[0]-'A'+1;
