6e7b52fe7049d6c0d41195f1c3a2a1bb90a985c9
gaurlakshya <69047854+gaurlakshya@users.noreply.github.com>
2022-06-25 16:55:55 +0530

    Time: 4 ms (44.46%), Space: 6.3 MB (25.40%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> us;
+        int ans=n;
+        while(true){
+            int temp=0;
+            while(ans){
+                temp+=pow(ans%10,2);
+                ans/=10;
+            }
+            ans=temp;
+            if(ans==1) return true;
+            if(us.count(ans))
+                return false;
+            us.insert(ans);
+        }
+    }
+};
