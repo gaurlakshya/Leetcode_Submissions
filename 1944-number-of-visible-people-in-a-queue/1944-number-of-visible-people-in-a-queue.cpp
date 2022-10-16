4697cdbb1fcfe156f1c59481f69e799a109319cb
gaurlakshya <69047854+gaurlakshya@users.noreply.github.com>
2022-10-16 08:35:31 +0530

    Time: 540 ms (5.88%), Space: 85 MB (16.24%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
@@ -6,13 +6,12 @@ public:
         stack<pair<int,int>> st;
         for(int i=0;i<n;i++){
             int height=heights[i];
-            while(!st.empty() && st.top().first < height){
+            while(!st.empty() && st.top().first<height){
                 int prev=st.top().second;
                 ans[prev]++;
                 st.pop();
             }
-            if(!st.empty())
-                ans[st.top().second]++;
+            if(!st.empty()) ans[st.top().second]++;
             st.push({height,i});
         }
         return ans;
