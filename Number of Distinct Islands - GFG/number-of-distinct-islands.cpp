cb1a8b696570eb0e1080b63d68622595a27766b8
gaurlakshya <69047854+gaurlakshya@users.noreply.github.com>
2022-12-27 13:41:12 +0530

    Added solution - LeetHub

--- Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
+++ Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
@@ -9,42 +9,49 @@ using namespace std;
 
 class Solution {
   public:
-    void dfs(int row,int col, vector<vector<int>> &vis,vector<vector<int>> &grid, vector<pair<int,int>>&vec
-    ,int row0,int col0){
-        vis[row][col]=1;
-        vec.push_back({row-row0,col-col0});
-        int n=grid.size();
+  void bfs(int r, int c, vector<vector<int>> &vis,vector<vector<int>>& grid, vector<pair<int,int>> &vec,int r0, int c0){
+      vis[r][c]=1;
+      queue<pair<int,int>> q;
+      q.push({r,c});
+      vec.push_back({r-r0,c-c0});
+      int delrow[]={-1,0,1,0};
+      int delcol[]={0,1,0,-1};
+      int n=grid.size();
         int m=grid[0].size();
-        int delrow[]={-1,0,1,0};
-        int delcol[] = {0,1,0,-1};
-        
-        for(int i=0;i<4;i++){
-            int nrow=row+delrow[i];
-            int ncol=col+delcol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]==1){
-                dfs(nrow,ncol,vis,grid,vec,row0,col0);
-            }
-        }
-    }
-    
-    
+      
+      while(!q.empty()){
+          int rf=q.front().first;
+          int cf=q.front().second;
+          q.pop();
+          for(int i=0;i<4;i++){
+              int nr=rf+delrow[i];
+              int nc=cf+delcol[i];
+              if(nr>=0 && nr<n && nc>=0 && nc<m && !vis[nr][nc] && grid[nr][nc]==1){
+                  vec.push_back({nr-r0, nc-c0});
+                  vis[nr][nc]=1;
+                  q.push({nr,nc});
+              }
+          }
+         
+      }
+  }
+  
     int countDistinctIslands(vector<vector<int>>& grid) {
         // code here
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
         set<vector<pair<int,int>>> st;
+        vector<vector<int>> vis(n,vector<int>(m,0));
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(!vis[i][j] && grid[i][j]==1){
                     vector<pair<int,int>> vec;
-                    dfs(i,j,vis,grid,vec,i,j);
+                    bfs(i,j,vis,grid,vec,i,j);
                     st.insert(vec);
                 }
             }
         }
-        
         return st.size();
     }
 };
