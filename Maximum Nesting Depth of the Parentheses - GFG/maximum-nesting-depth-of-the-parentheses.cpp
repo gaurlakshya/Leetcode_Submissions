0e63eaf9301d46e0ce5f99186e4d12c7572cad26
gaurlakshya <69047854+gaurlakshya@users.noreply.github.com>
2023-01-20 17:33:39 +0530

    Added solution - LeetHub

---
+++ Maximum Nesting Depth of the Parentheses - GFG/maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maxDepth(string s) {
+        // code here
+        int count=0;
+        int mx=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(') count++;
+            else if(s[i]==')') count--;
+            mx=max(mx,count);
+        }
+        return mx;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        Solution obj;
+        int ans = obj.maxDepth(s);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
